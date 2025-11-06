c7a3c93539201f90b4a9560a45f67b34212d3359
Sayfbenda <soufiane.bakouri.dev@gmail.com>
2025-11-06 22:05:38 +0100

    Add header to app

---
+++ src/component/header.js
@@ -0,0 +1,23 @@
+import '../css/header.css'
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <div>
+                    <img src="" alt=""/>
+                    <span>Soufiane</span>
+                </div>
+                <ul>
+                    <li>Menu</li>
+                    <li>Menu</li>
+                    <li>Menu</li>
+                    <li>Menu</li>
+                </ul>
+                <button>Test</button>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
